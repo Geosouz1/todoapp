29352396e32ac006b0fbb3599e85cbdcb17efb15
Geovani souza <geovani.m.soua@gmail.com>
2018-12-06 06:28:43 -0200

    Subindo projeo de curso

--- www/js/app.js
+++ www/js/app.js
@@ -1,26 +1,110 @@
-// Ionic Starter App
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('todoapp', ['ionic']);
 
-.run(function($ionicPlatform) {
+app.config(function($stateProvider, $urlRouterProvider){
+
+  $stateProvider.state('list',{
+    url:'/list',
+    templateUrl: 'templates/lista.html'
+  });
+  $stateProvider.state('new',{
+    url:'/new',
+    templateUrl: 'templates/novo.html',
+    controller: 'NovoCtrl'
+  });
+  $stateProvider.state('edit',{
+    url:'/edit/:indice',
+    templateUrl: 'templates/novo.html',
+    controller: 'EditCtrl'
+  });
+
+
+  $urlRouterProvider.otherwise('/list');
+});
+
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs).
-    // The reason we default this to hidden is that native apps don't usually show an accessory bar, at
-    // least on iOS. It's a dead giveaway that an app is using a Web View. However, it's sometimes
-    // useful especially with forms, though we would prefer giving the user a little more room
-    // to interact with the app.
-    if (window.cordova && window.Keyboard) {
-      window.Keyboard.hideKeyboardAccessoryBar(true);
+  
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+       cordova.plugins.Keyboard.disableScroll(true);
     }
 
     if (window.StatusBar) {
-      // Set the statusbar to use the default style, tweak this to
-      // remove the status bar on iOS or change it to use white instead of dark colors.
+
       StatusBar.styleDefault();
     }
   });
-})
+});
+
+
+app.controller('ListaCtrl', function($scope, $state){
+  //Aqui vem a implementação do meu controller:
+$scope.tarefas = tarefaService.lista();
+
+$scope.concluir = function(indice){
+$scope.tarefas[indice].feita=true;
+}
+
+$scope.apagar = function(indice){
+$scope.tarefaService.apagar;
+}
+
+$scope.editar = function(indice){
+  $state.go('edit', {indice : indice});
+}
+
+  $scope.mensagem = 'Hello World';
+});
+
+app.controller('NovoCtrl', function($scope,$state){ 
+$scope.tarefa = {
+    "texto" : $scope.texto,       //<input ng-model ="texto"...
+    "data" : new Date(),
+    "feita" : false
+  };
+
+  $scope.salvar = function(){
+
+    tarefaService.inserir($scope.tarefa);
+    $state.go('list');
+  }
+});
+app.controller('EditCtrl', function($scope,$state,$stateParams){ 
+  $scope.indice = $stateParams.indice;
+  $scope.tarefa = angular.copy(tarefaService.obtem($scope.indice));
+    $scope.salvar = function(){
+    
+      tarefaService.alterar($scope.indice, $scope.tarefa)
+     
+      tarefas[$scope.indice] = $scope.tarefa;
+      $state.go('list');
+    }
+  });
+
+  app.factory('TarefaService', function(){
+    var tarefas = [];
+    
+
+      return{
+        lista: function() {
+          return tarefas;
+        },
+        obtem: function(indice){
+          return tarefas[indice];
+        },
+        create: function(tarefa){
+          tarefas.push($scope.tarefa);
+        },
+        alterar: function(indice, tarefa){
+            tarefas[indice]=tarefa;
+        },
+        apagar: function(indice){
+          tarefas.splice(indice,1)
+        }
+      }
+
+  });
+  
